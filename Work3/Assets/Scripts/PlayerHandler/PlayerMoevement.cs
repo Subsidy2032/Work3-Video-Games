134a07c2440a1ccf7123f9a87ac27ed3e2c84245
Subsidy2032 <noam133m@gmail.com>
2024-09-22 09:36:08 +0300

    Initial board is ready

--- Work3/Assets/Scripts/PlayerHandler/PlayerMoevement.cs
+++ Work3/Assets/Scripts/PlayerHandler/PlayerMoevement.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private Vector3 movement;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-        movement = new Vector3(moveX, 0f, moveY);
+        movement = movement.normalized;
+    }
 
-        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+    void FixedUpdate()
+    {
+        rb.velocity = movement * moveSpeed;
     }
 }
